ab2bf46db7595c049bee04a7b8f2f1803503927f
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-10-28 17:17:44 +0100

    Creating a form for creating a book with Author and Title. Creating a submithandler to manage the input events and sending the postrequest with headers.

--- js/app.js
+++ js/app.js
@@ -5,12 +5,29 @@ const displayBooks = async () => {
   const data = await ( await fetch(apiUrl)).json()
   data.books.forEach(book => {
     const displayBookElement = document.createElement('div')
-    let html = `<p>The book ${book.title}, is written by ${book.auth}.`
+    let html = `<p>The book ${book.title}, is written by ${book.author}.`
     displayBookElement.innerHTML = html
     displayElement.appendChild(displayBookElement)
   });
 };
 
+const submitHandler = async (event) => {
+  event.preventDefault()
+  let author = event.target.author.value
+  let title = event.target.title.value
+  const response = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({title: title, author: author})
+  })
+  
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   displayBooks();
+  const createBookForm = document.getElementById('create-book')
+  createBookForm.addEventListener('submit', submitHandler)
 });
